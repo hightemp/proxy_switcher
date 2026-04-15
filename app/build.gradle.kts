229b1dca0529763bc9c11ece1c0b63fb4db6a297
hightemp <hightemp.unknown@gmail.com>
2026-04-15 16:57:16 +0300

    chore: bump version to 1.0.5 (versionCode=10005)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.hightemp.proxy_switcher"
         minSdk = 24
         targetSdk = 35
-        versionCode = 10004
-        versionName = "1.0.4"
+        versionCode = 10005
+        versionName = "1.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
